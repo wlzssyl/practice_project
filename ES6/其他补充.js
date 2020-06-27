5bf64413437f324d6e156e3100b2aa8f068b6ee6
wlzssyl <zhangboy7660@qq.com>
2020-06-27 13:11:04 +0800

    其他补充

---
+++ ES6/其他补充.js
@@ -0,0 +1,22 @@
+/**
+ * 其他内容
+ */
+/**
+ * 1.模块化
+ *  - 导出内容
+ *      export {a} ;
+ *  - 引入内容
+ *      import {a} from "./... .js"
+ *  - 导出默认对象
+ *      export default a(对象);
+ *  - 引入默认对象
+ *      import a（对象） from "./.... .js"
+ *      这里的变量a可以是b或者其他字面量
+ */
+/**************************************
+ * 2.promise   var p = Promise();
+ *   var p = Promise( (resolve,reject) => {
+ *     函数内容
+ * });
+ *  - Promise()后面括号里实际上是传入了一个函数
+ */
