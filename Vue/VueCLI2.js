d9ca32c42250f756ec6f58534a5a098b347a0aaf
wlzssyl <zhangboy7660@qq.com>
2020-07-16 17:29:06 +0800

    脚手架cli2

---
+++ Vue/VueCLI2.js
@@ -0,0 +1,10 @@
+/*************************************************8
+ * cli2初始化
+ *  - vue init webpack (这里是项目名称)
+ *  - 随后是一些设置
+ *     runtime-compiler
+ *     ESlint ， 即ES代码规范限制
+ *     unit tests ， 单元测试 （可选n）
+ *     e2e tests , 即end to end，端到端测试，一种自动化测试（n）
+ *  
+ */
