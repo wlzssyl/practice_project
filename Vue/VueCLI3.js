87a801a29c333558c2f73381bcc4b04af77e19d5
wlzssyl <zhangboy7660@qq.com>
2020-07-18 19:28:47 +0800

    cli3创建

--- Vue/VueCLI3.js
+++ Vue/VueCLI3.js
@@ -1,4 +1,8 @@
 /**********************************
 *1.cli3创建
   - vue create 项目名称
+   之后的就是配置选项，
+  - 可以选 Manually select features(手动选择配置)
+  - In dedicated config files(单独配置文件)
+  - save as a ... for future peojects (把这次配置存储为一个模板)
 */
