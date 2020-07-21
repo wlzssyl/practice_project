8a137eda8fd1a31dd6c8010b3d821882d2b81bdc
wlzssyl <zhangboy7660@qq.com>
2020-07-21 19:48:26 +0800

    组件懒加载

--- Vue/router/src/router/index.js
+++ Vue/router/src/router/index.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 // 从components文件夹获取组件
+//注意，import这种引入方式在打包的时候，组件会全部打包到一个app.js文件，这样会使文件太大访问性能不好，可以使用懒加载引入
 import Home from '../components/Home.vue'
 import About from '../components/About'
-import User from '../components/User.vue'
+//import User from '../components/User.vue'
+//懒加载， const Home = ()=>import('../components/Home')
+//懒加载会在打包时将该组件抽离到另一个js文件，只有访问了该组件才会加载js文件
+const User = () => import('../components/User')
 
 Vue.use(Router) //导入的Router要注册一下
 
