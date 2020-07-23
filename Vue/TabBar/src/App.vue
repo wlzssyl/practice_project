25b900edc5187a8ce27810bb133de8f16aa9b8c6
wlzssyl <zhangboy7660@qq.com>
2020-07-23 19:10:40 +0800

    TabBar组件开发

---
+++ Vue/TabBar/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <tab-bar>      
+      <tabbar-item>
+        <i class="iconfont icon-shouye" slot="item-icon"></i>
+        <div slot="item-text">首页</div>
+      </tabbar-item>
+      <tabbar-item>
+        <i class="iconfont icon-faxian" slot="item-icon"></i>
+        <div slot="item-text">分类</div>
+      </tabbar-item>  
+      <tabbar-item>  
+        <i class="iconfont icon-icon-test25" slot="item-icon"></i>
+        <div slot="item-text">购物车</div>
+      </tabbar-item>  
+      <tabbar-item>  
+        <i class="iconfont icon-shequ" slot="item-icon"></i>
+        <div slot="item-text">我的</div>
+      </tabbar-item>  
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBar from './components/tabar/TabBar'
+import TabbarItem from './components/tabar/TabbarItem'
+export default {
+  name: 'App',
+  components:{
+    TabBar,
+    TabbarItem
+  }
+}
+</script>
+
+<style>
+  @import "./assets/css/base.css";
+   @import './assets/font-icon/iconfont.css'
+</style>
---
+++ Vue/TabBar/src/components/tabar/TabbarItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="tabbar-item">
+    <slot name="item-icon"></slot>
+    <slot name="item-text"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'TabbarItem'
+}
+</script>
+
+<style>
+  .tabbar i{
+    color: #000;
+    font-size: 18px;
+  }
+  .tabbar-item{
+    flex: 1 0 auto;
+    margin-top: 5px;
+  } 
+  .tabbar-item div{
+    font-size: 12px;
+  }
+</style>
