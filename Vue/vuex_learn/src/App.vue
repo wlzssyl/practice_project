cfcdc46b49987940bba367c33fe13282424cb83b
wlzssyl <zhangboy7660@qq.com>
2020-07-27 17:57:53 +0800

    vuex- getters

--- Vue/vuex_learn/src/App.vue
+++ Vue/vuex_learn/src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
     <h2>APP组件</h2>
-    <p>{{$store.state.count}}</p>
-    <button @click="addition">+</button>
-    <button @click="substraction">-</button>
+    <p>{{$store.state.count}}的平方为{{$store.getters.powerCounter}}</p>
+    <button @click="addition">+1</button>
+    <button @click="substraction">-1</button>
+    <button @click="additionCount(3)">+3</button>
+    <p>{{$store.getters.someStudents(20)}}</p>
     <hello-vuex></hello-vuex>
   </div>
 </template>
@@ -22,7 +24,10 @@ export default {
       this.$store.commit('increment');
     },
     substraction(){
-      this.$store.commit('decrement')
+      this.$store.commit('decrement');
+    },
+    additionCount(num){
+      this.$store.commit();
     }
   }
 }
--- Vue/vuex_learn/src/store/index.js
+++ Vue/vuex_learn/src/store/index.js
@@ -7,7 +7,13 @@ Vue.use(Vuex)
 const store = new Vuex.Store(
   {
     state:{//状态
-      count:10
+      count:10,
+      students:[
+        {id:1001,name:'孙悟空',age:18},
+        {id:1002,name:'唐僧',age:28},
+        {id:1003,name:'猪八戒',age:19},
+        {id:1004,name:'沙和尚',age:20}
+      ]
     },
     mutations:{//方法,必须要通过这里对state中数据操作，才能监控到是哪个组件进行的操作
       increment(){
@@ -15,10 +21,22 @@ const store = new Vuex.Store(
       },
       decrement(){
         this.state.count--;
-      }
+      },
+      
     },
     actions:{},
-    getters:{},
+    getters:{//类比计算属性
+      powerCounter(state,getters){//getters中函数两个形参是固定的
+        return state.count*state.count;
+      },
+      someStudents(state,getters){
+        return function(age){ //这个返回的函数中的形参即为组件传来的数据
+          return state.students.filter((s) =>{
+            return s.age>=age;
+          });
+        }
+      }
+    },
     modules:{}
   }
 )
