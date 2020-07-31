e207ae5e59e0960edcba77bcb2fd73b0089a84b1
wlzssyl <zhangboy7660@qq.com>
2020-07-31 16:19:50 +0800

    axios拦截器

--- axios_demo/src/main.js
+++ axios_demo/src/main.js
@@ -19,31 +19,31 @@ new Vue({
  * 导入 import axios form 'asios'
  */
 //1.axios(config)简单实现
-axios({
-  url:'http://httpbin.org/', //请求地址
-  method:'get',//请求方式，get post delete put patch
-  params:{},  //与get配合使用，为get请求的网址进行参数拼接
-  data:{},  //与post配合使用，
-  baseURL:'', //这个是网址前缀，可以省去url中重复的前缀
-  timeout:3000  //超时设置
-  //还有其他设置可以查文档
-}).then(res => { //axios 是一个promise对象
-  console.log(res);
-})
-//2.axios.all([]),类似promise.all()
-axios.all([
-  axios({
-    url:'http://httpbin.org/'
-    //method不写默认get
-  }),
-  axios({
-    url:'http://httpbin.org/post',
-    method:'post'
-  })
-]).then(results => {
-  console.log(results);
-  console.log(results[0]);
-})
+// axios({
+//   url:'http://httpbin.org/', //请求地址
+//   method:'get',//请求方式，get post delete put patch
+//   params:{},  //与get配合使用，为get请求的网址进行参数拼接
+//   data:{},  //与post配合使用，
+//   baseURL:'', //这个是网址前缀，可以省去url中重复的前缀
+//   timeout:3000  //超时设置
+//   //还有其他设置可以查文档
+// }).then(res => { //axios 是一个promise对象
+//   console.log(res);
+// })
+// //2.axios.all([]),类似promise.all()
+// axios.all([
+//   axios({
+//     url:'http://httpbin.org/'
+//     //method不写默认get
+//   }),
+//   axios({
+//     url:'http://httpbin.org/post',
+//     method:'post'
+//   })
+// ]).then(results => {
+//   console.log(results);
+//   console.log(results[0]);
+// })
 //3.全局配置
 //axios.default.baseURL = '......'
 //axios.default.timeout = ...
