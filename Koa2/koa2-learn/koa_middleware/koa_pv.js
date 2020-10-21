18cdd39c2aa290baf797f36b4b26925e14f0194e
wlzssyl <zhangboy7660@qq.com>
2020-10-21 20:19:09 +0800

    redis的cmd命令说明

--- Koa2/koa2-learn/koa_middleware/koa_pv.js
+++ Koa2/koa2-learn/koa_middleware/koa_pv.js
@@ -9,3 +9,14 @@ module.exports = function() {
     await next()
   }
 }
+
+/****cmd命令行代码说明
+ *  - redis-cli
+ *    运行redis-cli 在redis目录下运行
+ *  - keys *   
+ *    查看所有session值
+ *  - get key值（即上面一行查到的值）
+ *    查看该session值的信息
+ *  - hget fix name 
+ *     这个是route里面的一个接口fix的查看方式，具体的话看菜鸟教程的相关命令学习吧
+ */
